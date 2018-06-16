71980bcc82b21f85d423e3639b9f7bb2bd052e0f
AnChu <chuvanan.cva@gmail.com>
2018-06-16 10:03:36 +0700

    Add some unit tests

---
+++ tests/testthat/test-idmcapi.R
@@ -0,0 +1,56 @@
+
+
+context('IDMC API')
+
+test_that('If get_countries() is broken', {
+    skip_on_cran()
+
+    res <- get_countries()
+    expect_is(res, 'data.frame')
+})
+
+test_that('If get_overview() is broken', {
+    skip_on_cran()
+
+    res <- get_overview('RUS')
+    expect_is(res, 'data.frame')
+})
+
+
+test_that('If get_confict_data() is broken', {
+    skip_on_cran()
+
+    res1 <- get_confict_data('RUS', year = 2015L)
+    expect_is(res1, 'data.frame')
+
+    res2 <- get_confict_data('RUS', year = c(2011L, 2015L))
+    expect_is(res2, 'data.frame')
+})
+
+test_that('If get_disaster_data() is broken', {
+    skip_on_cran()
+
+    res1 <- get_disaster_data('RUS', year = 2015L)
+    expect_is(res1, 'data.frame')
+
+    res2 <- get_disaster_data('RUS', year = c(2011L, 2015L))
+    expect_is(res2, 'data.frame')
+})
+
+
+test_that('If get_displacement_data() is broken', {
+    skip_on_cran()
+
+    res1 <- get_displacement_data('RUS', year = 2015L)
+    expect_is(res1, 'data.frame')
+
+    res2 <- get_displacement_data('RUS', year = c(2011L, 2015L))
+    expect_is(res2, 'data.frame')
+})
+
+test_that('If get_strata_data() is broken', {
+    skip_on_cran()
+
+    res <- get_strata_data('RUS')
+    expect_is(res, 'data.frame')
+})
